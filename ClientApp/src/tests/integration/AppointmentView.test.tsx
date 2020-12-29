e7e206c96757b4a6fc551e057649e7c94c33d644
Bartłomiej Chmiel <bachm44@gmail.com>
2020-12-29 17:32:54 +0100

    Add test

---
+++ ClientApp/src/tests/integration/AppointmentView.test.tsx
@@ -0,0 +1,10 @@
+import {screen} from '@testing-library/react';
+import React from 'react';
+import {AppointmentView} from '../../modules/client/views/AppointmentView/AppointmentView';
+import {render} from '../../test-utils';
+
+test('renders Appointment View component', () => {
+    render(<AppointmentView />);
+    const linkElement = screen.getByText(/DESCRIPTION/i);
+    expect(linkElement).toBeInTheDocument();
+});
