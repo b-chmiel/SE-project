c7dbf877910bd4328fc6373ea40b8f32a5447d07
Bartek Maciag <bartekmaciag@icloud.com>
2020-12-30 10:35:10 +0100

    add modal that we can add a car, but still doesn't communicate with api

---
+++ ClientApp/src/components/CarCard/AddCar.tsx
@@ -0,0 +1,58 @@
+import { Button, useDisclosure, Modal, ModalOverlay, FormControl, FormLabel, Input, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader } from "@chakra-ui/react";
+
+import React, { RefObject } from "react";
+
+
+function AddCarButton() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  
+    const initialRef: RefObject<any> = React.useRef()
+    const finalRef: RefObject<any> = React.useRef()
+  
+    return (
+      <>
+        <Button float="right" marginRight="20px" onClick={onOpen}>+ Add Car</Button>
+  
+        <Modal
+          initialFocusRef={initialRef}
+          finalFocusRef={finalRef}
+          isOpen={isOpen}
+          onClose={onClose}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Add Car</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>Model</FormLabel>
+                <Input ref={initialRef} placeholder="model" />
+              </FormControl>
+  
+              <FormControl mt={4}>
+                <FormLabel>Type</FormLabel>
+                <Input placeholder="type" />
+              </FormControl>
+
+
+              <FormControl mt={4}>
+                <FormLabel>Description</FormLabel>
+                <Input placeholder="description" />
+              </FormControl>
+              
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme="teal" mr={3}>
+                Save
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
+
+
+export default AddCarButton
