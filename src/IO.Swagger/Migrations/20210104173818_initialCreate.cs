4962a2383eb1b966ec6752ab6ddda60038236778
Baciejowski <k.baciejowski@gmail.com>
2021-01-04 20:15:59 +0100

    initial commit

---
+++ src/IO.Swagger/Migrations/20210104173818_initialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace IO.Swagger.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.CarId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
---
+++ src/IO.Swagger/desirj1i9l52n3Context.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using IO.Swagger.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace IO.Swagger
+{
+    public partial class desirj1i9l52n3Context : DbContext
+    {
+        public desirj1i9l52n3Context()
+        {
+        }
+
+        public desirj1i9l52n3Context(DbContextOptions<desirj1i9l52n3Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Server=ec2-54-247-71-245.eu-west-1.compute.amazonaws.com;Port=5432;User Id=nzsiiyxpwxqnef;Password=f62f533092bba8bc3de0a80bafb53ea3781db5b0a80629a031b062fd0dac80fa;Database=desirj1i9l52n3;SSL Mode=Require;TrustServerCertificate=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "en_US.UTF-8");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+        public DbSet<Car> Cars {get; set;}
+    }
+}
