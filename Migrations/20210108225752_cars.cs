c3e2fc6c6ed9c8231a294857b88e9584f51a2c56
Baciejowski <k.baciejowski@gmail.com>
2021-01-09 13:49:24 +0100

    Car i Diagnostic profile models again

---
+++ Migrations/20210108225752_cars.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace se_project.Migrations
+{
+    public partial class cars : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Guid = table.Column<string>(type: "text", nullable: true),
+                    UserType = table.Column<int>(type: "integer", nullable: false),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    LicensePlate = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    OwnerUsername = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.LicensePlate);
+                    table.ForeignKey(
+                        name: "FK_Cars_Users_OwnerUsername",
+                        column: x => x.OwnerUsername,
+                        principalTable: "Users",
+                        principalColumn: "Username",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DiagnosticProfiles",
+                columns: table => new
+                {
+                    LicensePlate = table.Column<int>(type: "integer", nullable: false),
+                    Engine = table.Column<string>(type: "text", nullable: true),
+                    Body = table.Column<string>(type: "text", nullable: true),
+                    LowVoltage = table.Column<string>(type: "text", nullable: true),
+                    Lighting = table.Column<string>(type: "text", nullable: true),
+                    Brakes = table.Column<string>(type: "text", nullable: true),
+                    Sensors = table.Column<string>(type: "text", nullable: true),
+                    Miscellaneous = table.Column<List<string>>(type: "text[]", nullable: true),
+                    Conditionig = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiagnosticProfiles", x => x.LicensePlate);
+                    table.ForeignKey(
+                        name: "FK_DiagnosticProfiles_Cars_LicensePlate",
+                        column: x => x.LicensePlate,
+                        principalTable: "Cars",
+                        principalColumn: "LicensePlate",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_OwnerUsername",
+                table: "Cars",
+                column: "OwnerUsername");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiagnosticProfiles");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
