a58d0abdab5fdb42c8cf720e0c210d491f4a4947
Bartłomiej Chmiel <bachm44@gmail.com>
2021-01-28 10:38:05 +0100

    Removed employee switch

--- ClientApp/src/modules/authorization/views/CreateUserView/CreateUserView.helpers.ts
+++
@@ -1,5 +0,0 @@
-import {UserType} from '../../helpers/AuthService.types';
-
-export function getUserType(userToggle: boolean): UserType {
-    return userToggle ? UserType.WORKSHOP_EMPLOYEE : UserType.CLIENT;
-}
